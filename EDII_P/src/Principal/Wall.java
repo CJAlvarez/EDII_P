f763c67f364aa70aec6144bc93f3e83c189aca7b
CJAlvarez <cristhianalvarez96@gmail.com>
2017-11-04 12:16:57 -0600

    DOC

--- EDII_P/src/Principal/Wall.java
+++ EDII_P/src/Principal/Wall.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Principal;
 
 /**
  *
- * @author USER PC
+ * @author CJ
  */
 public class Wall extends javax.swing.JFrame {
 
